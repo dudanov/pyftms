4e66b29433ecdc6628659d0258cf8f2eb07d2356
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2024-07-31 23:09:39 +0400

    simple example added

---
+++ examples/simple_with_control.py
@@ -0,0 +1,35 @@
+# Copyright 2024, Sergey Dudanov
+# SPDX-License-Identifier: Apache-2.0
+
+import asyncio
+import logging
+
+from pyftms import get_client_from_address, FtmsEvents, FitnessMachine
+
+ADDRESS = "29:84:5A:22:A4:11"
+
+logging.basicConfig(level=logging.DEBUG)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def on_event(event: FtmsEvents):
+    print(f"Event received: {event}")
+
+
+def on_disconnect(m: FitnessMachine):
+    print("Fitness Machine disconnected.")
+
+
+async def run():
+    async with await get_client_from_address(
+        ADDRESS, on_ftms_event=on_event, on_disconnect=on_disconnect
+    ) as c:
+        await c.start_resume()
+        await asyncio.sleep(30)
+        await c.set_target_speed(5)
+        await asyncio.sleep(30)
+        await c.stop()
+
+
+asyncio.run(run())
