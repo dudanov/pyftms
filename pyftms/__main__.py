06075b30f5d76e0aa93953df0a22112454f97670
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2024-09-14 15:55:33 +0400

    changed module program

--- pyftms/__main__.py
+++ pyftms/__main__.py
@@ -2,38 +2,34 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import asyncio
-import logging
 
-from .client import get_client_from_address
-from .client.backends import FtmsEvents
-
-ADDRESS = "FA:33:34:61:F4:68"
-
-logging.basicConfig(level=logging.DEBUG)
-
-_LOGGER = logging.getLogger(__name__)
-
-
-def on_event(event: FtmsEvents):
-    print(f"New event: {event}")
+from .client import discover_ftms_devices, get_client
 
 
 async def run():
-    async with await get_client_from_address(ADDRESS, on_ftms_event=on_event) as c:
-        properties = c.properties
-        settings = c.settings
+    print("Scanning for available FTMS devices...")
 
-        print(f"Device Info: {c.device_info}")
-        print(f"Supported: {c.supported_settings}")
-        print(f"Supported: {c.supported_properties}")
-        print(f"Available: {c.available_properties}")
+    lst = []
 
-        for _ in range(50):
-            print()
-            print(f"Properties: {properties}")
-            print(f"Settings: {settings}")
+    async for dev, machine_type in discover_ftms_devices(discover_time=5):
+        lst.append((dev, machine_type))
 
-            await asyncio.sleep(10)
+        print(
+            f"{len(lst)}. {machine_type.name}: name: {dev.name}, address: {dev.address}"
+        )
+
+    for dev, machine_type in lst:
+        print(
+            f"\nConnection to {machine_type.name}: name: {dev.name}, address: {dev.address}"
+        )
+
+        async with get_client(dev, machine_type) as c:
+            print(f" 1. Device Info: {c.device_info}")
+            print(f" 2. Supported settings: {c.supported_settings}")
+            print(f" 3. Supported properties: {c.supported_properties}")
+            print(f" 4. Available properties: {c.available_properties}")
+
+    print("\nDone.")
 
 
 asyncio.run(run())
