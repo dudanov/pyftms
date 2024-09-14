821038cf1db6d95819f0be405b933bf768f539bf
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2024-09-14 15:06:32 +0400

    discovering example

---
+++ examples/discovering.py
@@ -0,0 +1,14 @@
+# Copyright 2024, Sergey Dudanov
+# SPDX-License-Identifier: Apache-2.0
+
+import asyncio
+
+from pyftms import discover_ftms_devices
+
+
+async def run():
+    async for dev, machine_type in discover_ftms_devices():
+        print(f"Found {machine_type.name}: name: {dev.name}, address: {dev.address}")
+
+
+asyncio.run(run())
